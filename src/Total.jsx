67b196ce1127be373c98f070b56f50ae024c7a96
aa-r-ju <aarju3447@gmail.com>
2023-07-23 17:42:31 +0545

    feat:completed 1.1 exercise

---
+++ src/Total.jsx
@@ -0,0 +1,9 @@
+function Total ({exercises1,exercises2,exercises3}) {
+  let sum = exercises1 + exercises2 + exercises3
+    return(
+        <>
+<p> Number of exercises {sum}</p>
+        </>
+    )
+}
+export default Total;
