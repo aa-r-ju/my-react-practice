fd7892eabef68963f730b04fb426bfa58ecb3926
aa-r-ju <aarju3447@gmail.com>
2023-07-23 22:12:19 +0545

    feat:completed 1.5 exercise

--- src/Total.jsx
+++ src/Total.jsx
@@ -1,5 +1,9 @@
 function Total ({parts}) {
-  let sum = parts[0].exercises  + parts[1].exercises  + parts[2].exercises  
+  let sum = 0;
+  for(let i = 0; i < parts.length; i++) {
+   let  currentValue = parts[i]
+   sum += currentValue.exercises
+  }
     return(
         <>
 <p> Number of exercises {sum}</p>
